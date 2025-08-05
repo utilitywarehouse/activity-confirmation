a2d443099f0b3e44e16e10db45b395dc0f63fcbd
Ryan Richardson <rrichardson@uw.co.uk>
2025-08-05 16:18:01 +0100

    Add group and assignment

--- idp/Okta/activity-confirmation.tf
+++ idp/Okta/activity-confirmation.tf
@@ -1,4 +1,4 @@
-resource "okta_app_oauth" "activity-confirmation" {
+resource "okta_app_oauth" "activity_confirmation" {
   label       = "Activity Confirmation"
   issuer_mode = "CUSTOM_URL"
   type        = "browser"
@@ -13,3 +13,24 @@ resource "okta_app_oauth" "activity-confirmation" {
   token_endpoint_auth_method = "none" # toggles PKCE on
   pkce_required              = true
 }
+
+  resource "okta_group" "activity_confirmation" {
+      description               = "<group description>"
+      name                      = "<group name>"
+    }
+
+  resource "okta_group_rule" "activity_confirmation" {
+      name = "<rule name>"
+      expression_type   = "urn:okta:expression:1.0"
+      expression_value  = <<-EOT
+            (
+            (user.login != "")
+            )
+        EOT
+      status            = "ACTIVE"
+    }
+
+  resource "okta_app_group_assignment" "activity_confirmation" {
+      app_id            = okta_app_oauth.activity_confirmation.id
+      group_id          = okta_group.activity_confirmation.id
+    }
